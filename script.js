b7310fd68bc358592461122161711a7bf8f60f8a
Sinvaldo Pissinatti <112958318+SinvaldoPissinatti@users.noreply.github.com>
2022-09-14 17:03:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function redirect() {
+    window.location.href = 'https://www.google.com/search?q=never+gonna+give+you+up&oq=never+gonna+give+you+up&aqs=chrome..69i57.3483j0j1&sourceid=chrome&ie=UTF-8';
+}
